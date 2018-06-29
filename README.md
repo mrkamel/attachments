cea4b2f26d5aeab2c73f3353d4790575380ba4af
Benjamin Vetter <vetter@plainpicture.de>
2018-06-29 13:16:05 +0200

    Use 127.0.0.1 to fix travis

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Attachments
 
+[![Build Status](https://secure.travis-ci.org/mrkamel/attachments.png?branch=master)](http://travis-ci.org/mrkamel/attachments)
+
 Declarative and flexible attachments.
 
 ## Installation
--- spec/attachments/s3_driver_spec.rb
+++ spec/attachments/s3_driver_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Attachments::S3Driver do
     Attachments::S3Driver.new(Aws::S3::Client.new(
       access_key_id: "access_key_id",
       secret_access_key: "secret_access_key",
-      endpoint: "http://localhost:4567",
+      endpoint: "http://127.0.0.1:4567",
       region: "eu-central-1"
     ))
   end
